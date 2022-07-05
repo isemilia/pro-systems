925a96252fa53ef100cdb83bf5b90b87914fae8a
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-07-05 20:12:11 +0600

    Create sp-errors in setprisma

--- dist/js/causes.js
+++ dist/js/causes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('#causesSlider', {
         // Optional parameters
@@ -18,25 +17,4 @@ window.addEventListener('DOMContentLoaded', () => {
             },
         }
     });
-=======
-window.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('#causesSlider', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        // Navigation arrows
-        navigation: {
-          nextEl: '#causesSliderNext',
-          prevEl: '#causesSliderPrev',
-        },
-        slidesPerView: 1,
-        breakpoints: {
-            // when window width is >= 1440px
-            1440: {
-              slidesPerView: 4,
-              spaceBetween: 10
-            },
-        }
-    });
->>>>>>> 3d7ca160b00d315de9e357ed76164a9bb80b0620
 });
