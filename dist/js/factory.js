885a2f72b61364a7a49601caaf672657ca1f6157
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-07-05 19:07:27 +0600

    Create setprisma page

--- dist/js/factory.js
+++ dist/js/factory.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 window.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('#factorySlider', {
         // Optional parameters
@@ -17,4 +18,25 @@ window.addEventListener('DOMContentLoaded', () => {
             },
         }
     });
+=======
+window.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('#factorySlider', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        // Navigation arrows
+        navigation: {
+          nextEl: '#factorySliderNext',
+          prevEl: '#factorySliderPrev',
+        },
+        slidesPerView: 1,
+        breakpoints: {
+            // when window width is >= 1440px
+            1440: {
+              slidesPerView: 4,
+              spaceBetween: 10
+            },
+        }
+    });
+>>>>>>> 3d7ca160b00d315de9e357ed76164a9bb80b0620
 });
