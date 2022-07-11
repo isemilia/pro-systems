61fcd54c9524b73ad8d10c58f674d8a2aa568263
Akbota <85954194+slecnabota@users.noreply.github.com>
2022-07-12 00:22:52 +0600

    Add equipment & cart-product

---
+++ src/js/cp-about.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('#aboutSlider', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        // Navigation arrows
+        navigation: {
+          nextEl: '#aboutSliderNext',
+          prevEl: '#aboutSliderPrev',
+        },
+        slidesPerView: 1,  
+        initialSlide : 0,
+        centeredSlides:true,
+        breakpoints: {
+            // when window width is >= 1440px
+            1440: {
+              slidesPerView: 3,
+              spaceBetween: 170
+            },
+        }
+    });
+});
