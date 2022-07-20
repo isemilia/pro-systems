e8561373f23245821d26c6405c182214732ae62e
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-07-20 11:44:08 +0600

    Add datalogic page

--- src/js/cp-manf.js
+++ src/js/cp-manf.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('#manfCSISlider', {
         // Optional parameters
         direction: 'horizontal',
-        loop: true,
+        loop: false,
         // Navigation arrows
         navigation: {
           nextEl: '#manfCSISliderNext',
@@ -10,7 +10,8 @@ window.addEventListener('DOMContentLoaded', () => {
         },
         slidesPerView: 1,  
         initialSlide : 0,
-        centeredSlides:true,
+        // centeredSlides:true,
+        // centerInsufficientSlides: true,
         breakpoints: {
             // when window width is >= 1440px
             1440: {
