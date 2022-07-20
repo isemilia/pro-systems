0a42946bc263a9fe94b53cd57f0bec4b4b3ace71
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-07-20 17:54:07 +0600

    Add scales page

---
+++ src/js/cp-slider.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('#cpsSlider', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        // Navigation arrows
+        navigation: {
+          nextEl: '#cpsSliderNext',
+          prevEl: '#cpsSliderPrev',
+        },
+        slidesPerView: 1,
+        breakpoints: {
+            // when window width is >= 1440px
+            1440: {
+              slidesPerView: 3,
+              spaceBetween: 50
+            },
+        }
+    });
+});
