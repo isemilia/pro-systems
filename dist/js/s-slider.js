8535a11b37ebbdb65700a33667d0974984112802
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-07-21 13:06:59 +0600

    Add printf1 page

---
+++ dist/js/s-slider.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('#ssSlider', {
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '#ssNext',
+          prevEl: '#ssvPrev',
+        },
+        centeredSlides: true,
+        slideActiveClass: 's-slider__swiper-slide_active',
+        slidesPerGroup: 1,
+        breakpoints: {
+            1440: {
+                slidesPerView: 3,
+            }
+        }
+    });
+
+    const ssImgs = Array.from(document.querySelectorAll('[data-ss-img]'));
+    const ssLinks = Array.from(document.querySelectorAll('[data-ss-zoom]'));
+
+    ssImgs.forEach((img, i) => {
+        ssLinks[i].href = img.src;
+    });
+});
