f4cfed53e85514b9fd2421456799af4815c92eaf
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-07-25 20:55:33 +0600

    Add 1c retail page (promo & 1cfunc)

---
+++ dist/js/1c-func.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const swiper = new Swiper('#icFuncSlider', {
+        // Optional parameters
+      
+        // If we need pagination
+        pagination: {
+          el: '#icFuncPag',
+          clickable: true,
+        },
+      
+    });
+
+    const switchTitles = Array.from(document.querySelectorAll('[data-icfunc-title]'));
+    const switchDisplays = Array.from(document.querySelectorAll('[data-icfunc-display]'));
+
+    function removeAll() {
+        switchTitles.forEach((item) => {
+            item.classList.remove('ic-func__list-item_active');
+        });
+        switchDisplays.forEach((display) => {
+            display.classList.remove('ic-func__display_active');
+        });
+    };
+
+    switchTitles.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            removeAll();
+            item.classList.add('ic-func__list-item_active');
+            switchDisplays[i].classList.add('ic-func__display_active');
+        });
+    });
+
+
+    const icfuncMore = Array.from(document.querySelectorAll('[data-icfunc-more]'));
+    const icfuncTextMore = Array.from(document.querySelectorAll('[data-icfunc-text-more]'));
+
+    icfuncMore.forEach((item, i) => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            item.style.display = 'none';
+            icfuncTextMore[i].classList.add('ic-func__text-more_visible');
+        })
+    })
+})
