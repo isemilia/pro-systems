8c3c3a271975a3e66038f260008613ba0e629e40
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-08-26 14:41:35 +0600

    Responsive autom section in setretail page

---
+++ src/js/mob-switch.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('#mobSwitchSlider', {
+        loop: false,
+        // Navigation arrows
+        navigation: {
+            prevEl: '#mobSwitchPrev',
+            nextEl: '#mobSwitchNext',
+        },
+      
+      });
+})
