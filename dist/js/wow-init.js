adc30321971e3bd95a88f7baef8b4f229cd58148
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-09-02 11:57:40 +0600

    Initialize wow.js

---
+++ dist/js/wow-init.js
@@ -0,0 +1,9 @@
+let wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+}
+)
+wow.init();
