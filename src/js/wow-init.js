7475a8298539e339fcff4e372e9d2fe66338e7dc
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-09-02 17:44:10 +0600

    Animate 20 pages

--- src/js/wow-init.js
+++ src/js/wow-init.js
@@ -1,6 +1,7 @@
 let wow = new WOW(
     {
     boxClass:     'wow',      // default
+    animateClass: 'animate__animated', // default
     offset:       0,          // default
     mobile:       true,       // default
     live:         true        // default
